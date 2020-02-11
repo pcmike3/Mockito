b7f4edb2cb1cb9ee45ad539ebbb539e246cece2b
panchi <panchi@cmhi.chinamobile.com>
2020-02-11 15:42:59 +0800

    添加test6

--- src/test/java/com/pc/Lambda/LambdaDemoTest.java
+++ src/test/java/com/pc/Lambda/LambdaDemoTest.java
@@ -75,4 +75,10 @@ public class LambdaDemoTest {
         System.out.println(count);
         System.out.println(reduce);
     }
+
+    @Test
+    public void test6(){
+        List<Integer> list = Arrays.asList(1,2,4,5,6);
+        list.stream().map(x->x+1).forEach(System.out::println);
+    }
 }
