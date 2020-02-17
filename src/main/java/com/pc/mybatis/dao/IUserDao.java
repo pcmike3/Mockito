8f7a8145cbbb97bdaccf54a5428b5a6a4b58e52e
panchi <panchi@cmhi.chinamobile.com>
2020-02-17 13:05:21 +0800

    mybatis的增删改查操作

--- src/main/java/com/pc/mybatis/dao/IUserDao.java
+++ src/main/java/com/pc/mybatis/dao/IUserDao.java
@@ -6,4 +6,10 @@ import java.util.List;
 
 public interface IUserDao {
     List<User> findAll();
+    void saveUser(User user);
+    void updateUser(User user);
+    void deleteUser(Integer id);
+    User findById(Integer id);
+    List<User> findByName(String name);
+    Integer findTotal();
 }
--- src/test/java/com/pc/mybatis/test/mybatisTest.java
+++ src/test/java/com/pc/mybatis/test/mybatisTest.java
@@ -6,29 +6,107 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 import java.io.InputStream;
+import java.util.Date;
 import java.util.List;
 
+
+//1.读取配置文件
+//2.创建sqlSessionFactory工厂
+//3.使用工厂创建sqlSession对象
+//4.使用sqlSession创建Dao接口的代理对象
+//5.使用代理对象执行方法
+//6.释放资源
+
 public class mybatisTest {
-    public static void main(String[] args) throws Exception {
+
+    private IUserDao userDao;
+    private InputStream in;
+    private SqlSession session;
+
+    @Before
+    public void init() throws Exception{
         //1.读取配置文件
-        InputStream in = Resources.getResourceAsStream("sqlMapConfig.xml");
+        in = Resources.getResourceAsStream("sqlMapConfig.xml");
         //2.创建sqlSessionFactory工厂
         SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
         SqlSessionFactory factory = builder.build(in);
         //3.使用工厂创建sqlSession对象
-        SqlSession session = factory.openSession();
+        session = factory.openSession();
         //4.使用sqlSession创建Dao接口的代理对象
-        IUserDao userDao = session.getMapper(IUserDao.class);
-        //5.使用代理对象执行方法
+        userDao = session.getMapper(IUserDao.class);
+    }
+
+    @After
+    public void after() throws Exception {
+        session.commit();
+        session.close();
+        in.close();
+    }
+
+    @Test
+    public void testMybatis(){
+
         List<User> users = userDao.findAll();
+        for (User user : users) {
+            System.out.println(user);
+        }
+
+    }
+
+    @Test
+    public void testSave() {
+        User user = new User();
+        user.setAddress("厦门");
+        user.setBirthday(new Date());
+        user.setSex("男");
+        user.setUsername("zhaoqi");
+        //5.使用代理对象执行方法
+        userDao.saveUser(user);
+    }
+
+    @Test
+    public void testDelete() {
+        //5.使用代理对象执行方法
+        userDao.deleteUser(3);
+    }
+
+    @Test
+    public void testUpdate() {
+        User user=new User();
+        user.setId(1);
+        user.setUsername("liliu");
+        user.setSex("男");
+        user.setBirthday(new Date());
+        user.setAddress("河南");
+        //5.使用代理对象执行方法
+        userDao.updateUser(user);
+    }
+
+    @Test
+    public void testFindById() {
+
+        //5.使用代理对象执行方法
+        User user = userDao.findById(1);
+        System.out.println(user);
+    }
+
+    @Test
+    public void testFindByName() {
+
+        List<User> users = userDao.findByName("li%");
         for (User user:users){
             System.out.println(user);
         }
-        //6.释放资源
-        session.close();
-        in.close();
+    }
 
+    @Test
+    public void testFindTotal() {
+        Integer total = userDao.findTotal();
+        System.out.println(total);
     }
 }
